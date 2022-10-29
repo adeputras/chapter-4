f326d6be29c9f8bdd544d0d5264c14f0ebebcedb
Ade Putra Susilo <adeputrasusilo@Macbooks-MacBook-Pro.local>
2022-10-30 02:48:28 +0700

    update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/chapter-4/">
+      <BrowserRouter basename="/chapter-4">
         <App />
       </BrowserRouter>
     </Provider>
