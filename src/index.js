6770452e60c87e87c6f630cccc5f3195895658a2
Ade Putra Susilo <adeputrasusilo@Macbooks-MacBook-Pro.local>
2022-10-30 01:31:03 +0700

    update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <App />
       </HashRouter>
     </Provider>
