38effd8fce30dc9f90c6153af515bd30ac73b427
Ade Putra Susilo <adeputrasusilo@Macbooks-MacBook-Pro.local>
2022-10-30 01:24:19 +0700

    update

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter basename="https://adeputras.github.io/chapter-4">
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <App />
       </HashRouter>
     </Provider>
